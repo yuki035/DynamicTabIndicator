692f0429a67195dbc344622c1405cb51a78f83cf
yuki035 <yukiyuki1918@icloud.com>
2023-03-19 00:19:45 +0900

    add: 動的なタブを作成

--- DynamicTabIndicator/ContentView.swift
+++ DynamicTabIndicator/ContentView.swift
@@ -8,14 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentTab: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .top) {
+            TabView(selection: $currentTab) {
+                Color.red
+                    .opacity(0.2)
+                    .edgesIgnoringSafeArea(.all)
+                    .tag(0)
+                Color.yellow
+                    .opacity(0.2)
+                    .edgesIgnoringSafeArea(.all)
+                    .tag(1)
+                Color.green
+                    .opacity(0.2)
+                    .edgesIgnoringSafeArea(.all)
+                    .tag(2)
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .edgesIgnoringSafeArea(.all)
+            
+            TabBarView(currentTab: $currentTab)
         }
-        .padding()
+    }
+}
+
+struct TabBarView: View {
+    @Binding var currentTab: Int
+    @Namespace var namespace
+    var tabBarOptions: [String] = ["abcfafagaer", "defgagara", "ghigragea"]
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 20) {
+                ForEach(Array(zip(tabBarOptions.indices, tabBarOptions)), id: \.0) { index, name in
+                    TabBarItem(currentTab: $currentTab,
+                               namespace: namespace,
+                               tabBarItemName: name,
+                               tab: index)
+                }
+            }
+            .padding(.horizontal)
+        }
+        .background(Color.white)
+        .frame(height: 80)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct TabBarItem: View {
+    @Binding var currentTab: Int
+    let namespace: Namespace.ID
+    var tabBarItemName: String
+    var tab: Int
+    var body: some View {
+        Button {
+            currentTab = tab
+        } label: {
+            VStack {
+                Spacer()
+                Text(tabBarItemName)
+                if currentTab == tab {
+                    Color.black
+                        .frame(height: 2)
+                        .matchedGeometryEffect(id: "underline",
+                                               in: namespace,
+                                               properties: .frame)
+                } else {
+                    Color.clear.frame(height: 2)
+                }
+            }
+            .animation(.spring(), value: currentTab)
+        }
+        .buttonStyle(.plain)
     }
 }
 
