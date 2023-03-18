8de9831b3c37fefa3a6cfc28f6cb01c4089de630
yuki035 <yukiyuki1918@icloud.com>
2023-03-18 22:18:55 +0900

    Initial Commit

---
+++ DynamicTabIndicator/DynamicTabIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTabIndicatorApp.swift
+//  DynamicTabIndicator
+//
+//  Created by 森祐樹 on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTabIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
